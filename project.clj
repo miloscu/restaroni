ca55b25ee8b2293749d030975895c430f78f5c22
milos_cupara <milos.cupara@1x2network.com>
2023-02-23 11:57:53 +0100

    v1.0

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject restaroni "0.1.0-SNAPSHOT"
+(defproject restaroni "1.0.0"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :license {:name "CC0"
+            :url "https://creativecommons.org/publicdomain/zero/1.0/"}
   :dependencies [[cheshire "5.11.0"]
                  [clj-commons/fs "1.6.310"]
                  [clj-http "3.12.3"]
